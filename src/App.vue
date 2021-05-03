5c72fc043df5cb6224c5bbd8bc70a81416ef6188
Barış Gündüz <baris.gunduz@devstup.org>
2021-05-03 12:41:46 +0300

    basics

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,84 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+    <v-app id="inspire">
+        <v-navigation-drawer v-model="drawer" app>
+            <v-list-item>
+                <v-list-item-content>
+                    <v-list-item-title class="title">
+                        Vuetify Todo App
+                    </v-list-item-title>
+                    <v-list-item-subtitle>
+                        A nice todo app
+                    </v-list-item-subtitle>
+                </v-list-item-content>
+            </v-list-item>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+            <v-divider></v-divider>
 
-      <v-spacer></v-spacer>
+            <v-list dense nav>
+                <v-list-item
+                    v-for="item in items"
+                    :key="item.title"
+                    :to="item.to"
+                    link
+                >
+                    <v-list-item-icon>
+                        <v-icon>{{ item.icon }}</v-icon>
+                    </v-list-item-icon>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
+                    <v-list-item-content>
+                        <v-list-item-title>{{ item.title }}</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+        </v-navigation-drawer>
 
-    <v-main>
-      <router-view/>
-    </v-main>
-  </v-app>
+        <v-app-bar
+            app
+            color="primary"
+            dark
+            src="mountains.jpg"
+            prominent
+        >
+            <template v-slot:img="{ props }">
+                <v-img
+                    v-bind="props"
+                    gradient="to top right, rgba(19,84,122,.5), rgba(128,208,199,.8)"
+                ></v-img>
+            </template>
+
+            <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+            <v-app-bar-title>Title</v-app-bar-title>
+
+            <v-spacer></v-spacer>
+
+            <v-btn icon>
+                <v-icon>mdi-magnify</v-icon>
+            </v-btn>
+
+            <v-btn icon>
+                <v-icon>mdi-heart</v-icon>
+            </v-btn>
+
+            <v-btn icon>
+                <v-icon>mdi-dots-vertical</v-icon>
+            </v-btn>
+        </v-app-bar>
+
+        <v-main>
+            <router-view></router-view>
+        </v-main>
+    </v-app>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
-
-  data: () => ({
-    //
-  }),
+    data: () => ({
+        drawer: null,
+        items: [
+            { title: "Todo", icon: "mdi-format-list-checks", to: "/" },
+            { title: "About", icon: "mdi-help-box", to: "/about" },
+        ],
+    }),
 };
 </script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,23 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <div class="about pa-6">
+        <h1>Vuetify todo app</h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore
+            sit blanditiis non beatae. Sequi repellendus quas nemo. Minima dolor
+            assumenda maxime quos? Mollitia, possimus deserunt reprehenderit ut
+            laborum eveniet modi!
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore
+            sit blanditiis non beatae. Sequi repellendus quas nemo. Minima dolor
+            assumenda maxime quos? Mollitia, possimus deserunt reprehenderit ut
+            laborum eveniet modi!
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore
+            sit blanditiis non beatae. Sequi repellendus quas nemo. Minima dolor
+            assumenda maxime quos? Mollitia, possimus deserunt reprehenderit ut
+            laborum eveniet modi!
+        </p>
+    </div>
 </template>
